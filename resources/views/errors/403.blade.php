abbd561978d765ed0323901450b96fa637633ee0
User <htetmyataung@ubuntu.ubuntu-domain>
2019-11-06 20:42:06 -0800

    creating 403 error

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,5 +1,70 @@
 @extends('errors::minimal')
 
+/**
+ * Render an exception into an HTTP response.
+ *
+ * @param  \Illuminate\Http\Request  $request
+ * @param  \Exception  $exception
+ * @return \Illuminate\Http\Response
+ */
+public function render($request, Exception $exception)
+{
+    if ($this->isHttpException($exception)) {
+        if ($exception->getStatusCode() == 404) {
+            return response()->view('errors.' . '404', [], 404);
+        }
+    }
+ 
+    return parent::render($request, $exception);
+}
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page not found - 404</title>
+</head>
+<body>
+ 
+ 
+The page your looking for is not available
+ 
+</body>
+</html>
+
+public function render($request, Exception $exception)
+{
+    if ($this->isHttpException($exception)) {
+        if ($exception->getStatusCode() == 404) {
+            return response()->view('errors.' . '404', [], 404);
+        }
+         
+        if ($exception->getStatusCode() == 500) {
+            return response()->view('errors.' . '500', [], 500);
+        }
+    }
+ 
+    return parent::render($request, $exception);
+}
+
+public function render($request, Exception $exception)
+{
+    if ($this->isHttpException($exception)) {
+        if (view()->exists('errors.' . $exception->getStatusCode())) {
+            return response()->view('errors.' . $exception->getStatusCode(), [], $exception->getStatusCode());
+        }
+    }
+ 
+    return parent::render($request, $exception);
+}
+
+public function render($request, Exception $exception)
+{
+    if ($exception instanceof TestingHttpException) {
+        return response()->view('errors.testing');
+    }
+    return parent::render($request, $exception);
+}
+
 @section('title', __('Forbidden'))
 @section('code', '403')
 @section('message', __($exception->getMessage() ?: 'Forbidden'))
